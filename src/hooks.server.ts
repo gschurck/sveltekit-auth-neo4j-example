90dad85c2a8bc07dcad1f50b87e1405a0858c09d
Guillaume Schurck <g.schurck@gmail.com>
2023-01-26 02:07:15 +0100

    add neo4j adapter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,18 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
+import {SvelteKitAuth} from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import {GITHUB_ID, GITHUB_SECRET, NEO4J_PASSWORD, NEO4J_URI, NEO4J_USER} from "$env/static/private"
+import neo4j from "neo4j-driver"
+import {Neo4jAdapter} from "@next-auth/neo4j-adapter"
 
+const driver = neo4j.driver(
+    NEO4J_URI,
+    neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
+)
+
+const neo4jSession = driver.session()
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+    //@ts-expect-error
+    providers: [GitHub({clientId: GITHUB_ID, clientSecret: GITHUB_SECRET})],
+    //@ts-expect-error
+    adapter: Neo4jAdapter(neo4jSession),
 })
